d78feb3920213a8e0da2006dffca1e9273cabbef
ritaamehmeeti <litamehmeti12@gmail.com>
2025-01-09 20:46:40 +0100

    lesson32

---
+++ lesson32/main.js
@@ -0,0 +1,21 @@
+//var input =  document.getElementById('input_id');
+//var button = document.getElementById('btn_id');
+//var text = document.getElementById('text_id');
+
+//button.onclick=function(){
+//   text.innerHTML = input.value
+//};
+
+
+//var text = "this is the school of the digital schools";
+//var result = text.search('digital');
+//document.getElementById('result').innerHTML=result;
+
+//var text = "this is the school of the digital schools";
+//var result1 = text.search(/digital school/"another school");
+//document.getElementById('result1').innerHTML=result1;
+
+//var text = "Digital School is one of th top 10 schools in the world ";
+//var regex = /[0-9]/g;
+//document.getElementById('result3').innerHTML=text.match(regex);
+
