26a4a53f667cc96340d724510e55dc72b7310f0d
ritaamehmeeti <litamehmeti12@gmail.com>
2025-02-06 20:13:45 +0100

    part2

--- day 36/main.js
+++ day 36/main.js
@@ -4,24 +4,59 @@
 
 $('li').click(function(){
     alert("click");
-})
+});
+
 
 $('#btn').click(function(){
     console.log($('#h1').text());
     $('#h1').text("test1");
     $('#h1').append("test2");
-})
-
+});
 
 $('#btn2').on("click",function(){
-    console.log("the second button is clicked")
-})
-
+    console.log("the second button is clicked");
+});
 
 $('#myDiv').click(function(){
+
     $(this).addClass("clicked");
 })
+.find('span')
+.attr('title','Hover over me');
 
 $("#btn3").click(function(){
-    
+    //$(".hidden").show();
+    //$(".hidden").show(300);
+    $(".hidden").show('slow');
+})
+$("#btn4").click(function(){
+    $(".hidden").hide();
+});
+
+$("#square").click(function(){
+    $("#square").animate({
+        'width':'200px',
+        'height':'200px'
+    })
+})
+
+// $("#testing").click(function(){
+//     $("#testing").animate({
+//         'width':'200px',
+//         'height':'200px'
+//     })
+// })
+
+
+$(".testing").click(function(){
+    $(".testing").animate({
+        'left':'+=50px',
+        'opacity':0.25,
+        'fontSize':'30px'
+    },300,
+    function(){
+        console.log("animation is completed");
+    }
+
+    );
 })
